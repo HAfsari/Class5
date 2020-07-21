1430ca9b55d28d271a85fa776a21c5892e949f90
Afsarimisl <afsari@mislbd.com>
2020-07-21 21:04:02 +0600

    Add project files.

---
+++ Class5/Controllers/DataController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Class5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class5.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public IActionResult FileInfo()
+        {
+            //String dir = Directory.GetCurrentDirectory();
+            String dir = @"C:\Users\hejbu\Videos";
+            String[] file = Directory.GetFiles(dir);
+            List<FileList> list = new List<FileList>();
+            int sl = 1;
+
+            foreach (var i in file)
+            {
+                FileList fileList = new FileList();
+                fileList.id = sl;
+                fileList.name = Path.GetFileNameWithoutExtension(i);
+                fileList.type = Path.GetExtension(i);
+                fileList.path = Path.GetFullPath(i);
+
+                list.Add(fileList);
+                sl++;
+            }
+             return View(list);
+        }
+    }
+}
---
+++ Class5/Models/FileList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class5.Models
+{
+    public class FileList
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public String path { get; set; }
+        public string type { get; set; }
+    }
+}
